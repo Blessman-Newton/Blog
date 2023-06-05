f83238b2b58d86b527cfed50fb17f129796aea48
Blessman-Newton <blessmannewton0@gmail.com>
2023-06-05 16:04:58 +0100

    Add the config link in form,py file

--- blog/form.py
+++ blog/form.py
@@ -9,10 +9,17 @@ class AddPost(forms.ModelForm):
         """Meta definition for MODELNAMEform."""
 
         model = Post
-        fields = ('headline','body_text')
+        fields = ['headline','body_text','img']
         widgets = {
             'body_text': forms.Textarea(attrs={"placeholder":'Post Here',
                                                'style':'width: 100vh; height: 50vh'})
         }
 
-    
+class Image(forms.ModelForm):
+    """Form definition for MODELNAME."""
+
+    class Meta:
+        """Meta definition for MODELNAMEform."""
+
+        model = Post
+        fields = ['headline','body_text','img']
